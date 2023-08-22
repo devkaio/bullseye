1506e18f1907fca433102a95b45d6e8472caeb4b
Kaio C. de Oliveira <hello@kaio.dev>
2023-08-22 12:24:38 -0300

    kotlin android learning path - part two (build out the app)

--- app/src/main/java/dev/kaio/bullseye/MainActivity.kt
+++ app/src/main/java/dev/kaio/bullseye/MainActivity.kt
@@ -11,23 +11,33 @@ import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     private var sliderValue = 0
-    private var targetValue = Random.nextInt(1, 100)
+    private var targetValue = newTargetValue()
+    private var totalScore = 0
+    private var currentRound = 1
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        supportActionBar?.hide()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-
-        binding.targetTextView.text = targetValue.toString()
+        startNewGame()
 
         binding.hitMeButton.setOnClickListener {
             Log.i("Button Click Event", "Button clicked")
             showResult()
+            totalScore += pointsForCurrentRound()
+            binding.gameScoreTextView?.text = totalScore.toString()
         }
 
-        binding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+        binding.startOverButton?.setOnClickListener {
+            startNewGame()
+        }
+
+        binding.seekBar.setOnSeekBarChangeListener(object :
+            SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
                 sliderValue = progress
 
@@ -41,7 +51,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun showResult() {
-        val dialogTitle = getString(R.string.result_dialog_title)
+        val dialogTitle = alertTitle()
         val dialogMessage =
             getString(R.string.result_dialog_message, sliderValue, pointsForCurrentRound())
 //        val dialogMessage = "The slider value is $sliderValue"
@@ -50,14 +60,63 @@ class MainActivity : AppCompatActivity() {
 
         builder.setTitle(dialogTitle)
         builder.setMessage(dialogMessage)
-        builder.setPositiveButton(R.string.result_dialog_button_text) { dialog, _ -> dialog.dismiss() }
+        builder.setPositiveButton(R.string.result_dialog_button_text) { dialog, _ ->
+            dialog.dismiss()
+            targetValue = newTargetValue()
+            binding.targetTextView.text = targetValue.toString()
+
+            currentRound++
+            binding.gameRoundTextView?.text = currentRound.toString()
+        }
         builder.create().show()
 
     }
 
+    private fun differenceAmount() = abs(targetValue - sliderValue)
+    private fun newTargetValue() = Random.nextInt(1, 100)
+
     private fun pointsForCurrentRound(): Int {
         val maxScore = 100
-        val difference = abs(sliderValue - targetValue)
-        return maxScore - difference
+        val difference = differenceAmount()
+        var bonus = 0
+        if (difference == 0) {
+            bonus = 100
+        } else if (difference == 1) {
+            bonus = 50
+        }
+        return maxScore - difference + bonus
     }
+
+    private fun startNewGame() {
+        totalScore = 0
+        currentRound = 1
+        sliderValue = 50
+        targetValue = newTargetValue()
+
+        binding.gameScoreTextView?.text = totalScore.toString()
+        binding.gameRoundTextView?.text = currentRound.toString()
+        binding.targetTextView.text = targetValue.toString()
+        binding.seekBar.progress = sliderValue
+    }
+
+    private fun alertTitle(): String {
+        val difference = differenceAmount()
+        val title: String = when {
+            difference == 0 -> {
+                getString(R.string.alert_title_1)
+            }
+            difference < 5 -> {
+                getString(R.string.alert_title_2)
+            }
+            difference <= 10 -> {
+                getString(R.string.alert_title_3)
+            }
+            else -> {
+                getString(R.string.alert_title_4)
+            }
+        }
+
+        return title
+    }
+
 }
